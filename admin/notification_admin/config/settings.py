2cf72df15ad6ab95e5fc9b36a07de160a4d81c5c
Pavel Str <95212155+strbnm@users.noreply.github.com>
2022-05-11 11:56:06 +0300

    Feature/dev admin panel (#3)
    Create django project for admin panel

---
+++ admin/notification_admin/config/settings.py
@@ -0,0 +1,176 @@
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = int(os.environ.get('DEBUG', default=1))
+
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'debug_toolbar',
+    'django_extensions',
+    'notification.apps.NotificationConfig',
+    'tinymce',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', 5432),
+        'OPTIONS': {
+            'options': '-c search_path=public,content,notification'
+        }
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+
+    import socket # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[:-1] + '1' for ip in ips] + ['127.0.0.1', '10.0.2.2']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
+        },
+    },
+    'handlers': {
+        'debug-console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'filters': ['require_debug_true'],
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['debug-console'],
+            'propagate': False,
+        }
+    },
+}
+
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "640px",
+    "width": "960px",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+    "language": "ru_RU",
+}
---
+++ admin/notification_admin/notification/models.py
@@ -0,0 +1,135 @@
+import uuid
+
+from django.db import models
+from django.db.models import JSONField
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from tinymce.models import HTMLField
+
+
+class TimeStampedIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(_('дата создания'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('дата обновления'), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class TemplateKeys(models.TextChoices):
+    """Перечень кодов шаблонов"""
+
+    weekly_personal_mailing_movies = 'weekly_personal_mailing_movies', _('еженедельная персональная подборка фильмов')
+    monthly_personal_statistic_views = 'monthly_personal_statistic_views', _('ежемесячная персональная статистика просмотров')
+    daily_personal_statistic_likes = 'daily_personal_statistic_likes', _('ежедневная персональная статистика по лайкам рецензий')
+    email_confirmation = 'email_confirmation', _('подтверждение адреса электронной почты при регистрации')
+
+
+class AdapterTypes(models.TextChoices):
+    """Типы адаптеров для отправки уведомлений"""
+    email = 'email'
+    sms = 'sms'
+    websocket = 'websocket'
+
+
+class Template(TimeStampedIDModel):
+    """Шаблоны уведомлений"""
+    title = models.CharField(_('наименование шаблона уведомления'), max_length=120, blank=False, null=True)
+    subject = models.CharField(_('тема уведомления'), max_length=120, blank=True, null=True)
+    from_email = models.CharField(_('электронная почта отправителя'), max_length=120, blank=True, null=True)
+    html_template = HTMLField(_('содержание html-шаблона'), blank=True, null=True)
+    plain_text = models.TextField(_('содержание текстового шаблона'), blank=True, null=True)
+    is_html = models.BooleanField(default=False)
+    is_text = models.BooleanField(default=False)
+    template_key = models.CharField(_('вид шаблона'), choices=TemplateKeys.choices, max_length=50)
+    adapter = models.CharField(
+        _('используемый адаптер для отправки уведомления'),
+        choices=AdapterTypes.choices,
+        max_length=50,
+        default=AdapterTypes.email,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+
+        self.updated_at = timezone.now()
+        return super().save(*args, **kwargs)
+
+    def _get_body(self):
+        if self.is_text:
+            return self.plain_text
+
+        return self.html_template
+
+    def __str__(self):
+        return f'Template <{self.title}> for sending by <{self.adapter}>'
+
+    class Meta:
+        db_table = "notification\".\"templates"
+        verbose_name = _('шаблон уведомления')
+        verbose_name_plural = _('шаблоны уведомлений')
+        managed = True
+
+
+class NotificationStatus(models.TextChoices):
+    to_immediate_send = 'To immediate send', _('для немедленной отправки')
+    to_scheduled_send = 'To scheduled send', _('для отправки планировщиком')
+    in_processing = 'In processing', _('в процессе обработки')
+    completed = 'Completed', _('успешно завершена')
+    cancelled = 'Cancelled', _('отменена')
+
+
+class NotificationPriority(models.TextChoices):
+    high = 'high', _('высокий приоритет')
+    medium = 'medium', _('средний приоритет')
+    low = 'low', _('низкий приоритет')
+
+
+class RepeatFrequency(models.TextChoices):
+    daily = 'daily', _('ежедневно')
+    weekly = 'weekly', _('еженедельно')
+    monthly = 'monthly', _('ежемесячно')
+
+
+class MailingTask(TimeStampedIDModel):
+    """Рассылка"""
+    title = models.CharField(_('наименование рассылки'), max_length=120, blank=False, null=True)
+    status = models.CharField(
+        _('статус рассылки'),
+        max_length=250,
+        choices=NotificationStatus.choices,
+        default=NotificationStatus.to_scheduled_send,
+    )
+    is_promo = models.BooleanField(_('рекламная рассылка'), default=True)
+    priority = models.CharField(
+        _('приоритет рассылки'),
+        max_length=30,
+        choices=NotificationPriority.choices,
+        default=NotificationPriority.low,
+    )
+    template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, verbose_name=_('шаблон уведомления'))
+    context = JSONField(_('контекст рассылки'), default=dict())
+    scheduled_datetime = models.DateTimeField(_('плановые дата и время рассылки'), blank=True, null=True)
+    repeat_frequency = models.CharField(
+        _('периодичность рассылки'),
+        max_length=20,
+        choices=RepeatFrequency.choices,
+    )
+    execution_datetime = models.DateTimeField(_('фактическое время завершения рассылки'), blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+
+        self.updated_at = timezone.now()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Mailing <{self.title}>'
+
+    class Meta:
+        db_table = "notification\".\"mailing_tasks"
+        verbose_name = _('задача рассылки')
+        verbose_name_plural = _('задачи рассылки')
+        managed = True
