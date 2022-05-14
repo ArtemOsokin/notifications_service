e3a7d63bf374392f426e650307cfc2344ced0450
Artem Osokin <aptem21@mail.ru>
2022-05-15 01:09:50 +0300

    add scheduler

---
+++ scheduler/src/config.py
@@ -0,0 +1,23 @@
+import os
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_DSN: dict = {
+        'dbname': os.environ.get('DB_NAME'),
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASSWORD'),
+        'host': os.environ.get('DB_HOST'),
+        'port': os.environ.get('DB_PORT'),
+        'options': '-c search_path=public,content,notification'
+    }
+
+    NOTIFICATION_API_URL: str = 'http://localhost:8080/api/v1/'
+    AUTH_API_URL: str = 'http://localhost:8000/api/v1/admin/'
+    BATCH_SIZE = 100
+    SLEEP_TIME = 5
+    BACKOFF_MAX_TIME = 10
+    CHUNK_SIZE = 10
+
+
+settings = Settings()
---
+++ scheduler/src/db.py
@@ -0,0 +1,15 @@
+from config import settings
+import psycopg2
+from psycopg2.extras import DictCursor
+
+
+class Extractor:
+
+    def extract(self, sql):
+        with psycopg2.connect(dsn=settings.DB_DSN, cursor_factory=DictCursor) as connection:
+            with connection.cursor() as cur:
+                cur.execute(sql)
+                try:
+                    data = [dict(item) for item in cur.fetchall()]
+                except psycopg2.ProgrammingError:
+                    data = None
